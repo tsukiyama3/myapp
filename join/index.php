16a2724aaf4000087d48047bd2548f54dbdb6572
築山航平 <tsukiyama.mail@gmail.com>
2019-12-30 22:22:51 +0900

    index.phpのheader部分

--- join/index.php
+++ join/index.php
@@ -8,6 +8,21 @@
   <link rel="stylesheet" href="../styles.css">
 </head>
 <body>
+
+<!-- header -->
+
+<header>
+
+  <div class="header-left">
+    <h1>MyApp</h1>
+  </div>
+  <div class="header-right">
+    <a href="#">ログイン</a>
+  </div>
+
+</header>
+
+<!-- header -->
   
 </body>
 </html>
