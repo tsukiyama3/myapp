fd4d9b4ecdf33bb08150ee22a6a9fd34f35cf073
築山航平 <tsukiyama.mail@gmail.com>
2020-01-04 22:48:57 +0900

    update.php初回のコミット

---
+++ update.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>myapp</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+<body>
+  
+</body>
+</html>
