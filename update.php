30a3f18860621351a9f93754b4b9eb4b8d2ec4fd
築山航平 <tsukiyama.mail@gmail.com>
2020-01-04 23:13:08 +0900

    update.phpのphp部分

--- update.php
+++ update.php
@@ -1,3 +1,52 @@
+<?php
+
+session_start();
+require('dbconnect.php');
+
+// 正しく$_REQUEST['id']が指定されている場合の処理
+if (isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])) {
+
+  // $_REQUEST['id']を$idに代入
+  $id = $_REQUEST['id'];
+
+  // $repostsのsql文
+  $reposts = $db->prepare('SELECT * FROM posts WHERE id=?');
+  $reposts->execute(array($id));
+  $repost = $reposts->fetch();
+
+  // フォームが空じゃない場合の処理
+  if (!empty($_POST)) {
+
+    // エラー処理
+    if ($_POST['title'] === '') {
+      $error['title'] = 'blank';
+    }
+
+    if ($_POST['impre'] === '') {
+      $error['impre'] = 'blank';
+    }
+
+    // エラーがない場合の処理
+    if (empty($error)) {
+      // sql文
+      $statement = $db->prepare('UPDATE posts SET title=?, impre=? WHERE id=?');
+      $statement->execute(array(
+        $_POST['title'],
+        $_POST['impre'],
+        $id
+      ));
+      header('Location: index.php');
+      exit();
+    }
+
+  }
+
+} else { // $_REQUEST['id']が正しくない場合の処理
+  header('Location: index.php');
+  exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -39,10 +88,13 @@
     
       <div class="content-left">
         <h2>タイトル</h2>
+        <?php if ($error['title'] === 'blank'): ?>
+          <p class="error">* タイトルを入力してください。</p>
+        <?php endif; ?>
       </div>
 
       <div class="content-right">
-        <input type="text" name="title" size="50" value="">
+        <input type="text" name="title" size="50" value="<?php print(htmlspecialchars($repost['title'], ENT_QUOTES)); ?>">
       </div>
     
     </div>
@@ -51,10 +103,13 @@
     
       <div class="content-left">
         <h2>感想</h2>
+        <?php if ($error['impre'] === 'blank'): ?>
+          <p class="error">* 感想を入力してください。</p>
+        <?php endif; ?>
       </div>
     
       <div class="content-right">
-        <textarea name="impre" cols="50" rows="10"></textarea>
+        <textarea name="impre" cols="50" rows="10"><?php print(htmlspecialchars($repost['impre'], ENT_QUOTES)); ?></textarea>
       </div>
 
     </div>
