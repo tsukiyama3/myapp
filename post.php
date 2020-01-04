92df457ad40e76b77c41f9fca583c00c0e8ea4ec
築山航平 <tsukiyama.mail@gmail.com>
2020-01-04 22:32:47 +0900

    post.phpのheader部分

--- post.php
+++ post.php
@@ -9,6 +9,22 @@
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
+
+<!-- header -->
+
+<header>
+
+  <div class="header-left">
+    <h1>MyApp</h1>
+  </div>
+
+  <div class="header-right">
+    <a href="index.php"><i class="fas fa-backward"></i>戻る</a>
+  </div>
+
+</header>
+
+<!-- header -->
   
 </body>
 </html>
